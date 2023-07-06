e3961e8e7e818bf2faafd89f11be37d71b694c96
Souradip Ghosh <souradip1212@gmail.com>
2023-07-06 17:13:35 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+// Define your API endpoint
+app.get('/api/greeting', (req, res) => {
+  const name = req.query.name || 'Anonymous';
+  const greeting = `Hello, ${name}!`;
+  res.json({ message: greeting });
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
