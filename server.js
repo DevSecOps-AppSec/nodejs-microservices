9a72ee41fa91da95198c64cf7f132577d345d329
Souradip Ghosh <souradip1212@gmail.com>
2023-07-06 17:29:15 +0530

    Added few more GET endpoints

--- server.js
+++ server.js
@@ -4,15 +4,29 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-// Define your API endpoint
+// Define your API endpoints
 app.get('/api/greeting', (req, res) => {
   const name = req.query.name || 'Anonymous';
   const greeting = `Hello, ${name}!`;
   res.json({ message: greeting });
 });
 
+app.get('/api/time', (req, res) => {
+  const currentTime = new Date().toLocaleTimeString();
+  res.json({ time: currentTime });
+});
+
+app.get('/api/users', (req, res) => {
+  const users = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Jane Smith' },
+    { id: 3, name: 'Bob Johnson' }
+  ];
+  res.json({ users });
+});
+
 // Start the server
-const port = 80;
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
